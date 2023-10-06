89970908c42808bb17288b1d4887f4c41d069bfb
Salama Ayuba <salamaayuba111@gmail.com>
2023-10-06 20:19:42 +0000

    Modified

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-
-'''A python module that returns the summation of two float numbers'''
+'''Task 0's module.
+'''
 
 
 def add(a: float, b: float) -> float:
-    '''A function that returns the summation of two float numbers'''
-    return a + b
-
-
-if __name__ == '__main__':
-
-    print(add(1.11, 2.22) == 1.11 + 2.22)
-    print(add.__annotations__)
-
+    '''Adds two floating-point numbers.
+    '''
+    return a + b 
